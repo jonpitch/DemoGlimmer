f01d32f694d9066544cbc88bd30a2e26612a6ba2
jonpitch <jon.pitcherella@gmail.com>
2017-12-29 13:15:33 -0500

    add regenerator-runtime

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { ComponentManager, setPropertyDidChange } from '@glimmer/component';
 import initializeCustomElements from '@glimmer/web-component';
 import App from './main';
+import 'regenerator-runtime/runtime';
 
 const app = new App();
 const containerElement = document.getElementById('app');
